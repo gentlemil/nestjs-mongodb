8343f113ebd8005793b17e968b31ceb9b838e420
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-03-05 19:05:09 +0100

    config mongoose, create User schema, module, service and controller, init createUser method

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nestjs_tutorial'),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+const PORT = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(PORT, () =>
+    console.log(`Server is listening on port: ${PORT}`),
+  );
 }
 bootstrap();
